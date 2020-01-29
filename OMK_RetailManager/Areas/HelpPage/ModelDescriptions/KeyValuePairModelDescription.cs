d9f36abb7cfb791e7d19352b5e9cc8e785cdc0c4
Owen <eng.owenmurithi@gmail.com>
2020-01-29 19:08:07 +0300

    User creation and authentication
    We are creating users using APIs to authenticate, using Postman

---
+++ OMK_RetailManager/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace OMK_RetailManager.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ OMK_RetailManager/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace OMK_RetailManager.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
