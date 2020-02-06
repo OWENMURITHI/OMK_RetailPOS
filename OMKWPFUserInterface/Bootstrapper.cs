2fd52697402dba4779c1d370b1f48a459b7b8570
Owen <eng.owenmurithi@gmail.com>
2020-02-06 20:02:42 +0300

    Adds WPF Object with MVVM
    Adds our WPF interface project and configures it with calibra micro and also using small container

---
+++ OMKWPFUserInterface/Bootstrapper.cs
@@ -0,0 +1,51 @@
+﻿using Caliburn.Micro;
+using OMKWPFUserInterface.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OMKWPFUserInterface
+{
+   public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer _container = new SimpleContainer();
+
+        public Bootstrapper()
+            {
+                Initialize();
+            }
+        
+      
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }  
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("Viewmodel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
+      
+    }
+}
